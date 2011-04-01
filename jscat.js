3dca37f7d802c7f4f0fa06fdb15fea9da2f725ab
David Fifield <david@bamsoftware.com>
2011-04-01 04:16:29 -0700

    Add listening and selecting to jscat.

--- jscat.js
+++ jscat.js
@@ -8,9 +8,32 @@ var io = {
 };
 
 var connector = {
+	"selector": java.nio.channels.Selector.open(),
+
 	"wait_for_event": function() {
+		var n;
+		do {
+			n = connector.selector.select();
+		} while (n == 0);
+		var key = connector.selector.selectedKeys().iterator().next();
+		var ev = {};
+		if (key.readyOps() & java.nio.channels.SelectionKey.OP_ACCEPT) {
+			ev.type = "accept";
+			ev.sd = key.channel().socket().accept();
+			ev.address = ev.sd.getInetAddress().getHostAddress();
+			ev.port = ev.sd.getPort();
+			/* userdata */
+			return ev;
+		}
 	},
-	"listen": function(port, userdata) {
+	"listen": function(address, port, userdata) {
+		var ssc = java.nio.channels.ServerSocketChannel.open();
+		ssc.configureBlocking(false);
+		var s = ssc.socket();
+		s.bind(java.net.InetSocketAddress(port));
+		ssc.register(connector.selector, java.nio.channels.SelectionKey.OP_ACCEPT);
+		/* userdata */
+		return s;
 	},
 	"connect": function(address, port, userdata) {
 	},
@@ -24,19 +47,23 @@ var connector = {
 
 io.print("jscat starting.")
 
+var LOCAL_ADDRESS = "0.0.0.0";
 var LOCAL_PORT = 9998;
 
 var DIRECTORY_ADDRESS = "localhost";
 var DIRECTORY_PORT = 9999;
 
-var peer = {};
+var peers = {};
 
-connector.listen(LOCAL_PORT);
+var l = connector.listen(LOCAL_ADDRESS, LOCAL_PORT);
+print("Listening socket: " + l);
 
 while (true) {
 	var ev = connector.wait_for_event();
+	print("ev: type:" + ev.type + " sd:" + ev.sd);
 	switch (ev.type) {
 	case "accept":
+		io.print("Connection from " + ev.address + ":" + ev.port + ".");
 		connector.connect(DIRECTORY_ADDRESS, DIRECTORY_PORT, ev.sd);
 		break;
 	case "connect":
@@ -50,8 +77,10 @@ while (true) {
 		connector.send(ev.userdata, ev.data);
 		break;
 	case "close":
+		var peer = peers[ev.sd];
 		connector.close(peer[ev.sd]);
-		peer[ev.sd] = peer[peer[ev.sd]] = undefined;
+		delete peers[ev.sd];
+		delete peers[peer];
 		break;
 	case "error":
 		io.quit();
