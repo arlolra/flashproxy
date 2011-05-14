58f18a34323f3a6e136f42a722c71a077eccd0d4
David Fifield <david@bamsoftware.com>
2011-05-13 19:04:28 -0700

    Use uniform logging in the facilitator that prepends a timestamp.

--- facilitator.py
+++ facilitator.py
@@ -7,10 +7,13 @@ import cgi
 import re
 import sys
 import socket
+import time
 from collections import deque
 
 DEFAULT_ADDRESS = "0.0.0.0"
 DEFAULT_PORT = 9002
+
+LOG_DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
 
 def usage(f = sys.stdout):
     print >> f, """\
@@ -18,14 +21,20 @@ Usage: %(progname)s <OPTIONS> [HOST] [PORT]
 Flash bridge facilitator: Register client addresses with HTTP POST requests
 and serve them out again with HTTP GET. Listen on HOST and PORT, by default
 %(addr)s %(port)d.
-  -h, --help  show this help.\
+  -h, --help          show this help.
+  -l, --log FILENAME  write log to FILENAME (default stdout).\
 """ % {
     "progname": sys.argv[0],
     "addr": DEFAULT_ADDRESS,
     "port": DEFAULT_PORT,
 }
 
+log_file = sys.stdout
 REGS = deque()
+
+def log(msg):
+    print >> log_file, (u"%s %s" % (time.strftime(LOG_DATE_FORMAT), msg)).encode("UTF-8")
+    log_file.flush()
 
 class Reg(object):
     def __init__(self, af, host, port):
@@ -81,9 +90,9 @@ class Reg(object):
 def add_reg(reg):
     if reg not in list(REGS):
         REGS.append(reg)
-        print "Registration " + str(reg) + " added. Registrations: " + str(len(REGS))
+        log("Registration " + str(reg) + " added. Registrations: " + str(len(REGS)))
     else:
-        print "Registration " + str(reg) + " already present. Registrations: " + str(len(REGS))
+        log("Registration " + str(reg) + " already present. Registrations: " + str(len(REGS)))
 
 def fetch_reg():
     """Get a client registration, or None if none is available."""
@@ -93,43 +102,45 @@ def fetch_reg():
 
 class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
     def do_GET(self):
-        print "From " + str(self.client_address) + " received: GET:",
+        line = "From " + str(self.client_address) + " received: GET:"
         reg = fetch_reg()
         if reg:
-            print "Handing out " + str(reg) + ". Clients: " + str(len(REGS))
+            line += " Handing out " + str(reg) + ". Clients: " + str(len(REGS))
             self.request.send(str(reg))
         else:
-            print "Registration list is empty"
+            line += " Registration list is empty"
+        log(line)
 
     def do_POST(self):
-        print "From " + str(self.client_address) + " received: POST:",
         data = self.rfile.readline().strip()
-        print data + " :",
+        log("From " + str(self.client_address) + " received: POST: " + data + " :")
         try:
             vals = cgi.parse_qs(data, False, True)
         except ValueError, e:
-            print "Syntax error in POST:", str(e)
+            log("Syntax error in POST: %s" % str(e))
             return
 
         client_specs = vals.get("client")
         if client_specs is None or len(client_specs) != 1:
-            print "In POST: need exactly one \"client\" param"
+            log("In POST: need exactly one \"client\" param")
             return
         val = client_specs[0]
 
         try:
             reg = Reg.parse(val, self.client_address[0])
         except ValueError, e:
-            print "Can't parse client \"%s\": %s" % (val, str(e))
+            log("Can't parse client \"%s\": %s" % (val, str(e)))
             return
 
         add_reg(reg)
 
-opts, args = getopt.gnu_getopt(sys.argv[1:], "h", ["help"])
+opts, args = getopt.gnu_getopt(sys.argv[1:], "hl:", ["help", "log="])
 for o, a in opts:
     if o == "-h" or o == "--help":
         usage()
         sys.exit()
+    elif o == "-l" or o == "--log":
+        log_file = open(a, "a")
 
 if len(args) == 0:
     address = (DEFAULT_ADDRESS, DEFAULT_PORT)
@@ -151,7 +162,7 @@ class Server(SocketServer.ThreadingMixIn, BaseHTTPServer.HTTPServer):
 # Setup the server
 server = Server(address, Handler)
 
-print "Starting Facilitator on " + str(address) + "..."
+log("Starting Facilitator on " + str(address) + "...")
 
 try:
     server.serve_forever()
