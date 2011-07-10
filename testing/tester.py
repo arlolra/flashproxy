d19e8dbec32ceff107b5897e43546a833954d2c8
ellitron <jdellit@stanford.edu>
2011-07-09 18:42:24 -0700

    deleting a bunch of old testing scripts

--- testing/tester.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-import os
-import sys
-
-for i in range(int(sys.argv[1])):
-	print "Running test " + str(i) + "..."
-	os.system("cat fac_test_input_" + str(i) + " | ncat 127.0.0.1 9002")
-
