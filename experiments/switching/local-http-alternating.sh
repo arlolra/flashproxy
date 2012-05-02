arlolra/flashproxy

commit a9dce418b568aee67076abb5727c2dcbae73e835

Update switching experiment for JS proxy.

experiments/switching/local-http-alternating.sh
@@ -9,7 +9,7 @@
 
 PROFILE_1=flashexp1
 PROFILE_2=flashexp2
-PROXY_URL="http://127.0.0.1:8000/swfcat.swf?facilitator=127.0.0.1:9002&ratelimit=off"
+PROXY_URL="http://127.0.0.1:8000/embed.html?facilitator=127.0.0.1:9002&ratelimit=off"
 DATA_FILE_NAME="$FLASHPROXY_DIR/dump"
 OUTPUT_FILENAME="$1"
 
@@ -41,7 +41,7 @@ PIDS_TO_KILL+=($!)
 visible_sleep 5
 
 echo "Start connector."
-"$FLASHPROXY_DIR"/connector.py --facilitator 127.0.0.1 >/dev/null &
+"$FLASHPROXY_DIR"/connector.py --register --facilitator 127.0.0.1:9002 >/dev/null &
 PIDS_TO_KILL+=($!)
 visible_sleep 1
 
@@ -54,8 +54,9 @@ PIDS_TO_KILL+=($!)
 visible_sleep 2
 
 echo "Start socat."
-"$SOCAT" TCP-LISTEN:2000,reuseaddr,fork SOCKS4A:127.0.0.1:dummy:0,socksport=9001 &
+"$FLASHPROXY_DIR"/connector.py >/dev/null &
 PIDS_TO_KILL+=($!)
+echo $'POST / HTTP/1.0\r\n\r\nclient=:9000' | socat - TCP-CONNECT:127.0.0.1:9002
 visible_sleep 2
 
 

experiments/switching/local-http-constant.sh
@@ -9,7 +9,7 @@
 
 PROFILE_1=flashexp1
 PROFILE_2=flashexp2
-PROXY_URL="http://127.0.0.1:8000/swfcat.swf?facilitator=127.0.0.1:9002&ratelimit=off"
+PROXY_URL="http://127.0.0.1:8000/embed.html?facilitator=127.0.0.1:9002&ratelimit=off"
 DATA_FILE_NAME="$FLASHPROXY_DIR/dump"
 OUTPUT_FILENAME="$1"
 
@@ -35,14 +35,19 @@ echo "Start web server."
 "$THTTPD" -D -d "$FLASHPROXY_DIR" -p 8000 &
 PIDS_TO_KILL+=($!)
 
+echo "Start websockify."
+"$WEBSOCKIFY" -v 8001 127.0.0.1:8000 >/dev/null &
+PIDS_TO_KILL+=($!)
+
 echo "Start facilitator."
-"$FLASHPROXY_DIR"/facilitator.py -d --relay 127.0.0.1:8000 >/dev/null &
+"$FLASHPROXY_DIR"/facilitator.py -d --relay 127.0.0.1:8001 >/dev/null &
 PIDS_TO_KILL+=($!)
 visible_sleep 5
 
 echo "Start connector."
-"$FLASHPROXY_DIR"/connector.py --facilitator 127.0.0.1 >/dev/null &
+"$FLASHPROXY_DIR"/connector.py >/dev/null &
 PIDS_TO_KILL+=($!)
+echo $'POST / HTTP/1.0\r\n\r\nclient=:9000' | socat - TCP-CONNECT:127.0.0.1:9002
 visible_sleep 1
 
 echo "Start browser."

experiments/switching/remote-tor-alternating.sh
@@ -9,7 +9,7 @@
 
 PROFILE_1=flashexp1
 PROFILE_2=flashexp2
-PROXY_URL="http://127.0.0.1:8000/swfcat.swf?facilitator=127.0.0.1:9002&ratelimit=off"
+PROXY_URL="http://127.0.0.1:8000/embed.html?facilitator=127.0.0.1:9002&ratelimit=off"
 DATA_FILE_NAME="$FLASHPROXY_DIR/dump"
 OUTPUT_FILENAME="$1"
 
@@ -38,8 +38,9 @@ PIDS_TO_KILL+=($!)
 visible_sleep 5
 
 echo "Start connector."
-"$FLASHPROXY_DIR"/connector.py --facilitator 127.0.0.1 >/dev/null &
+"$FLASHPROXY_DIR"/connector.py >/dev/null &
 PIDS_TO_KILL+=($!)
+echo $'POST / HTTP/1.0\r\n\r\nclient=:9000' | socat - TCP-CONNECT:127.0.0.1:9002
 visible_sleep 1
 
 echo "Start Tor."

experiments/switching/remote-tor-constant.sh
@@ -9,7 +9,7 @@
 
 PROFILE_1=flashexp1
 PROFILE_2=flashexp2
-PROXY_URL="http://127.0.0.1:8000/swfcat.swf?facilitator=127.0.0.1:9002&ratelimit=off"
+PROXY_URL="http://127.0.0.1:8000/embed.html?facilitator=127.0.0.1:9002&ratelimit=off"
 DATA_FILE_NAME="$FLASHPROXY_DIR/dump"
 OUTPUT_FILENAME="$1"
 
@@ -37,8 +37,9 @@ PIDS_TO_KILL+=($!)
 visible_sleep 5
 
 echo "Start connector."
-"$FLASHPROXY_DIR"/connector.py --facilitator 127.0.0.1 >/dev/null &
+"$FLASHPROXY_DIR"/connector.py >/dev/null &
 PIDS_TO_KILL+=($!)
+echo $'POST / HTTP/1.0\r\n\r\nclient=:9000' | socat - TCP-CONNECT:127.0.0.1:9002
 visible_sleep 1
 
 echo "Start Tor."

commit 3c7c11e61793509deb090216feb370d979f50e40

Fix socat line, accidentally broken in a9dce418.

experiments/switching/local-http-alternating.sh
@@ -54,9 +54,8 @@ PIDS_TO_KILL+=($!)
 visible_sleep 2
 
 echo "Start socat."
-"$FLASHPROXY_DIR"/connector.py >/dev/null &
+"$SOCAT" TCP-LISTEN:2000,reuseaddr,fork SOCKS4A:127.0.0.1:dummy:0,socksport=9001 &
 PIDS_TO_KILL+=($!)
-echo $'POST / HTTP/1.0\r\n\r\nclient=:9000' | socat - TCP-CONNECT:127.0.0.1:9002
 visible_sleep 2
 
 