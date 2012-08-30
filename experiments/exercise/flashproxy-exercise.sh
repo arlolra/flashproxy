c6b0433653472a155af86ea5ac6c71bff6cce4fc
David Fifield <david@bamsoftware.com>
2012-08-30 14:48:10 -0700

    Show the console log iff flashproxy-exercise fails.
    This is to send cron mail when the script fails, but not otherwise.

--- experiments/exercise/flashproxy-exercise.sh
+++ experiments/exercise/flashproxy-exercise.sh
@@ -6,7 +6,4 @@
 DATE=$(date +"%Y-%m-%d-%H:%M")
 LOG="log-$DATE"
 
-# To get cron mail:
-# ./exercise.sh 2>&1 | tee "$LOG"
-# To not get cron mail:
-./exercise.sh &> "$LOG"
+(./exercise.sh &> "$LOG") || cat "$LOG"
