03d253168bb6fce49bcbd0dc77b5fe930dece8b7
David Fifield <david@bamsoftware.com>
2012-09-14 22:01:09 -0700

    Don't disable for Safari 6.0 and later.
    Safari 6 now has the newer hybi/RFC 6455 type of WebSocket. I just
    tested and was able to bootstrap through Safari for the first time.
    
    I did not find a nice in-browser way to test for the websocket type,
    short of User-Agent sniffing.

--- flashproxy.js
+++ flashproxy.js
@@ -843,10 +843,11 @@ function flashproxy_should_disable() {
         }
     }
 
-    if (ua.match(/\bsafari\b/i) && !ua.match(/\bchrome\b/i)) {
-        /* Disable on Safari because it doesn't have the hybi/RFC type of
-           WebSockets. */
-        puts("Disable because User-Agent is Safari.");
+    if (ua.match(/\bsafari\b/i) && !ua.match(/\bchrome\b/i)
+        && !ua.match(/\bversion\/[6789]\./i)) {
+        /* Disable before Safari 6.0 because it doesn't have the hybi/RFC type
+           of WebSockets. */
+        puts("Disable because User-Agent is Safari before 6.0.");
         return true;
     }
 
