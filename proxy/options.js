1f20f598494fba8612472545272d721f8f45468c
Alexandre Allaire <alexandre.allaire@mail.mcgill.ca>
2012-12-04 21:33:33 -0500

    Adding opt-in page html and javascript.
    Adding some functional html and javascript for
    the opt-in page (ticket #7063). There is no CSS
    for the page yet.

---
+++ proxy/options.js
@@ -0,0 +1,66 @@
+/* This is the javascript for the opt-in page. It sets/deletes
+   a cookie which controls whether the flashproxy javascript
+   code should run or disable itself. */
+
+var COOKIE_NAME = "flashproxy";
+/* In seconds. */
+var COOKIE_LIFETIME = 60 * 60 * 24 * 365;
+
+window.addEventListener("load", function () {
+
+    /* This checks if cookies are enabled in the browser.
+       document.cookie has special behavior, if cookies
+       are disabled it will not retain any values stored in it. */
+    function cookies_enabled() {
+        /*Not supported in all browsers.*/
+        if (navigator.cookieEnabled) {
+            return true;
+        } else if (navigator.cookieEnabled === undefined) {
+            document.cookie = "test";
+            if (document.cookie.indexOf("test") !== -1)
+                return true;
+        }
+        return false;
+    }
+
+    /* Updates the text telling the user what his current setting is.
+       According to the DOM level 2 specification,
+       http://www.w3.org/TR/DOM-Level-2-HTML/html.html#ID-8747038,
+       document.cookie is always defined and non-null, so this function
+       is safe if cookies are disabled. */
+    function update_setting_text() {
+        var setting = document.getElementById("setting");
+        var prefix = "<p>Your current setting is: ";
+
+        if (document.cookie.indexOf(COOKIE_NAME) !== -1) {
+            setting.innerHTML = prefix + "use my browser as a proxy. " +
+                                         "Click no below to change your setting.</p>";
+        } else {
+            setting.innerHTML = prefix + "do not use my browser as a proxy. " +
+                                         "Click yes below to change your setting.</p>";
+        }
+    }
+
+    function set_cookie() {
+        document.cookie = COOKIE_NAME + "=; max-age=" + COOKIE_LIFETIME;
+    }
+
+    function del_cookie() {
+        document.cookie = COOKIE_NAME + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    }
+
+    if (cookies_enabled()) {
+        var buttons = document.getElementById("buttons");
+        buttons.addEventListener("click", update_setting_text);
+        document.getElementById("yes").addEventListener("click", set_cookie);
+        document.getElementById("no").addEventListener("click", del_cookie);
+        buttons.style.display = "block";
+        update_setting_text();
+    } else {
+        document.getElementById("cookies_disabled").style.display = "block";
+        /* Manually set the text here as it refers to the buttons,
+           which won't show up if cookies are disabled. */
+        document.getElementById("setting").innerHTML = "<p>Your current setting is: " +
+                                                       "do not use my browser as a proxy.</p>";
+    }
+});
