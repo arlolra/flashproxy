5597f222cc9c4c2080c5d5668446758882972304
David Fifield <david@bamsoftware.com>
2013-05-18 19:24:39 -0700

    go fmt.

--- appspot/fp.go
+++ appspot/fp.go
@@ -1,32 +1,32 @@
 package fp
 
 import (
-  "fmt"
-  "net/http"
+	"fmt"
+	"net/http"
 
-  "appengine"
-  "appengine/urlfetch"
+	"appengine"
+	"appengine/urlfetch"
 )
 
 const BASE = "https://fp-facilitator.org/reg/"
 
 func ipHandler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "%s", r.RemoteAddr)
+	fmt.Fprintf(w, "%s", r.RemoteAddr)
 }
 
 func regHandler(w http.ResponseWriter, r *http.Request) {
-  c := appengine.NewContext(r)
-  blob := r.URL.Path[5:]
-  client := urlfetch.Client(c)
-  _, err := client.Get(BASE + blob)
-  if err != nil {
-    http.Error(w, err.Error(), http.StatusInternalServerError)
-    return
-  }
-  fmt.Fprintf(w, "Thanks.")
+	c := appengine.NewContext(r)
+	blob := r.URL.Path[5:]
+	client := urlfetch.Client(c)
+	_, err := client.Get(BASE + blob)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintf(w, "Thanks.")
 }
 
 func init() {
-  http.HandleFunc("/ip", ipHandler)
-  http.HandleFunc("/reg/", regHandler)
+	http.HandleFunc("/ip", ipHandler)
+	http.HandleFunc("/reg/", regHandler)
 }
