3ebabec5f7cd8754d6d194dd44e43ecbd725e93f
David Fifield <david@bamsoftware.com>
2013-06-07 13:03:35 -0700

    Put flashproxy-exercise logs in a subdirectory.

--- experiments/exercise/flashproxy-exercise.sh
+++ experiments/exercise/flashproxy-exercise.sh
@@ -3,7 +3,9 @@
 # Usage (for example in crontab for hourly tests):
 #   0 *    *   *   *   cd /path/flashproxy-exercise && ./flashproxy-exercise.sh
 
+LOGDIR=log
 DATE=$(date +"%Y-%m-%d-%H:%M")
-LOG="log-$DATE"
+LOG="$LOGDIR/log-$DATE"
 
+mkdir -p "$LOGDIR"
 (./exercise.sh &> "$LOG") || cat "$LOG"
