0eccc984c50bae6736b58a2e75ed3af8e41a7b1e
George Kadianakis <desnacked@riseup.net>
2013-09-18 16:11:42 +0300

    Send flashproxy pools that include transport information.
    For now, we only do the "websocket" transport.

--- proxy/flashproxy.js
+++ proxy/flashproxy.js
@@ -562,6 +562,7 @@ function FlashProxy() {
 
         /* Flash proxy protocol revision. */
         params = [["r", "1"]];
+        params.push(["transport", "websocket"]);
         /* Clients we're currently handling. */
         for (var i = 0; i < this.proxy_pairs.length; i++)
             params.push(["client", format_addr(this.proxy_pairs[i].client_addr)]);
