15e76754dfa01aa77de3c4c906887976c0b479d9
David Fifield <david@bamsoftware.com>
2014-02-01 17:21:36 -0800

    Raise ValueError on non-integer port in format_addr.

--- flashproxy/util.py
+++ flashproxy/util.py
@@ -116,6 +116,7 @@ def format_addr(addr):
         else:
             host_str = u"%s" % host
     if port is not None:
+        port = int(port)
         if not (0 < port <= 65535):
             raise ValueError("port must be between 1 and 65535 (is %d)" % port)
         port_str = u":%d" % port
