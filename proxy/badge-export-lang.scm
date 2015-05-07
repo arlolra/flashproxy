ba0a517d25bd94bfba5424a4b3309801e5dfd5ff
David Fifield <david@bamsoftware.com>
2015-05-07 09:03:34 -0700

    Make the badge XCF nicer.
    Threshold-alpha the text layers (some pixels were semi-transparent).
    Move the border to its own layer.

--- proxy/badge-export-lang.scm
+++ proxy/badge-export-lang.scm
@@ -6,13 +6,15 @@
 (define (export lang)
   (let* ((image (car (gimp-file-load RUN-NONINTERACTIVE xcf-filename xcf-filename)))
          (shine-layer (car (gimp-image-get-layer-by-name image "shine")))
+         (border-layer (car (gimp-image-get-layer-by-name image "border")))
          (text-layer (car (gimp-image-get-layer-by-name image (string-append "text-" lang))))
          (output-filename (string-append "badge-" lang ".png")))
     ; Turn off all layers.
     (for-each (lambda (x) (gimp-item-set-visible x FALSE))
               (vector->list (cadr (gimp-image-get-layers image))))
-    ; Except the shine and the wanted text.
+    ; Except the shine, border, and the wanted text.
     (gimp-item-set-visible shine-layer TRUE)
+    (gimp-item-set-visible border-layer TRUE)
     (gimp-item-set-visible text-layer TRUE)
     (gimp-image-merge-visible-layers image CLIP-TO-IMAGE)
     (file-png-save RUN-NONINTERACTIVE image
